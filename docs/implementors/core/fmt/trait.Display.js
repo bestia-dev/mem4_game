7928bf7f8e122152d288d819d6abc46ea0dc218b
Luciano Bestia <luciano.bestia@gmail.com>
2019-07-05 22:40:24 +0800

    docs

---
+++ docs/implementors/core/fmt/trait.Display.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["mem4_common"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"mem4_common/enum.GameStatus.html\" title=\"enum mem4_common::GameStatus\">GameStatus</a>",synthetic:false,types:["mem4_common::GameStatus"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
--- docs/source-files.js
+++ docs/source-files.js
@@ -1,5 +1,5 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["mem4"] = {"name":"","dirs":[],"files":["divcardmoniker.rs","divgridcontainer.rs","divplayeractions.rs","divplayersandscores.rs","divrulesanddescription.rs","gamedata.rs","lib.rs","rootrenderingcomponent.rs","statusinviteaskbegin.rs","statusinviteasked.rs","statusinviteasking.rs","statusplayagain.rs","statusplaybefore1card.rs","statusplaybefore2card.rs","statustaketurnbegin.rs","websocketcommunication.rs","websocketreconnect.rs"]};
+sourcesIndex["mem4"] = {"name":"","dirs":[],"files":["divcardmoniker.rs","divfordebugging.rs","divgridcontainer.rs","divplayeractions.rs","divplayersandscores.rs","divrulesanddescription.rs","gamedata.rs","lib.rs","rootrenderingcomponent.rs","statusinviteaskbegin.rs","statusinviteasked.rs","statusinviteasking.rs","statusplayagain.rs","statusplaybefore1stcard.rs","statusplaybefore2ndcard.rs","statustaketurnbegin.rs","websocketcommunication.rs","websocketreconnect.rs"]};
 sourcesIndex["mem4_common"] = {"name":"","dirs":[],"files":["lib.rs"]};
 sourcesIndex["mem4_server"] = {"name":"","dirs":[],"files":["main.rs"]};
 createSourceSidebar();
